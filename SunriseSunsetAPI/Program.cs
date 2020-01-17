f11bdb4e28addb82e791928b18827b5b1243614c
Julia Aasmaa <my_e_10@hotmail.com>
2020-01-17 11:23:29 +0200

    commit

---
+++ SunriseSunsetAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SunriseSunsetAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userCategoryUrl = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=today";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(userCategoryUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine(sunMode.Status);
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunrise}");
+                Console.WriteLine($"Sunset: {sunMode.Results.Sunset}");
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SunriseSunsetAPI/SunMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunriseSunsetAPI
+{
+    class SunMode
+    {
+        public string Status { get; set; }
+        public Results Results { get; set; }
+
+    }
+
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+    }
+}
